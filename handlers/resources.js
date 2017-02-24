1d7d281df1f01ee327d7f0bc3faf72b5d3c3513c
Joshua Colvin <joshua@joshuacolvin.net>
2017-02-23 22:42:19 -0500

    Add resource handlers

---
+++ handlers/resources.js
@@ -0,0 +1,21 @@
+'use strict'
+
+exports.find = function (request, reply) {
+  reply('find')
+}
+
+exports.createOne = function (request, reply) {
+  reply('create one')
+}
+
+exports.findOne = function (request, reply) {
+  reply('find one')
+}
+
+exports.updateOne = function (request, reply) {
+  reply('update one')
+}
+
+exports.deleteOne = function (request, reply) {
+  reply('delete one')
+}
