455754fe38063f6d97cd0c94dc4cf49cc3b70f49
Joshua Colvin <joshua@joshuacolvin.net>
2017-02-25 11:26:23 -0500

    Add comments handler

---
+++ handlers/comments.js
@@ -0,0 +1,17 @@
+'use strict'
+
+exports.find = function (request, reply) {
+  reply('find comment')
+}
+
+exports.create = function (request, reply) {
+  reply('create one')
+}
+
+exports.updateOne = function (request, reply) {
+  reply('update one')
+}
+
+exports.deleteOne = function (request, reply) {
+  
+}
